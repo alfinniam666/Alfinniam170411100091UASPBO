cfd97d06a06cb347a92ec21b244e41c1465f4e89
ALFINNIAM-170411100091 <44285027+alfinniam666@users.noreply.github.com>
2018-12-20 23:27:23 +0700

    Pygame & asset game
    File : game.py

---
+++ game.py
@@ -0,0 +1,391 @@
+from __future__ import division
+import pygame
+import random
+from os import path
+
+## data suara dan gambar
+img_dir = path.join(path.dirname(__file__), 'gambar')
+sound_folder = path.join(path.dirname(__file__), 'suara')
+##Tinggi & Lebar Screen 
+WIDTH = 720
+HEIGHT = 680
+FPS = 60
+
+POWERUP_TIME = 5000
+
+BAR_LENGTH = 100
+BAR_HEIGHT = 10
+ 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)  
+# inisialisasi pygame dan memanggil window
+pygame.init()
+pygame.mixer.init()  ## For sound
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("FREE SHOOTER (EDIT FROM SPACE Shooter by ANAS & ALFIN)")
+clock = pygame.time.Clock()     
+font_name = pygame.font.match_font('arial')
+
+def main_menu():
+    global screen
+
+    title = pygame.image.load(path.join(img_dir, "hills.png")).convert()
+    title = pygame.transform.scale(title, (WIDTH, HEIGHT), screen)
+
+    screen.blit(title, (0,0))
+    pygame.display.update()
+
+    while True:
+        ev = pygame.event.poll()
+        if ev.type == pygame.KEYDOWN:
+            if ev.key == pygame.K_RETURN:
+                break
+            elif ev.key == pygame.K_q:
+                pygame.quit()
+                quit()
+        elif ev.type == pygame.QUIT:
+                pygame.quit()
+                quit() 
+        else:
+            draw_text(screen, "TEKAN [ENTER] UNTUK BERMAIN", 30, WIDTH/2, HEIGHT/2)
+            draw_text(screen, "TEKAN [Q] UNTUK KELUAR DARI GAME ", 30, WIDTH/2, (HEIGHT/2)+40)
+            pygame.display.update()
+
+    #pygame.mixer.music.stop()
+    ready = pygame.mixer.Sound(path.join(sound_folder,'getready.ogg'))
+    ready.play()
+    screen.fill(BLACK)
+    draw_text(screen, "GET READY!", 40, WIDTH/2, HEIGHT/2)
+    pygame.display.update()
+    
+
+def draw_text(surf, text, size, x, y):
+    ## untuk menampilkan score
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, BLUE)        
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
+
+
+def draw_shield_bar(surf, x, y, pct):
+    # if pct < 0:
+    #     pct = 0
+    pct = max(pct, 0) 
+    ## moving them to top
+    # BAR_LENGTH = 100
+    # BAR_HEIGHT = 10
+    fill = (pct / 100) * BAR_LENGTH
+    outline_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
+    fill_rect = pygame.Rect(x, y, fill, BAR_HEIGHT)
+    pygame.draw.rect(surf, GREEN, fill_rect)
+    pygame.draw.rect(surf, WHITE, outline_rect, 2)
+
+
+def draw_lives(surf, x, y, lives, img):
+    for i in range(lives):
+        img_rect= img.get_rect()
+        img_rect.x = x + 30 * i
+        img_rect.y = y
+        surf.blit(img, img_rect)
+
+def newmeteor():
+    meteor_element = meteor()
+    all_sprites.add(meteor_element)
+    meteors.add(meteor_element)
+
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, center, size):
+        pygame.sprite.Sprite.__init__(self)
+        self.size = size
+        self.image = explosion_anim[self.size][0]
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.frame = 0 
+        self.last_update = pygame.time.get_ticks()
+        self.frame_rate = 75
+
+    def update(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > self.frame_rate:
+            self.last_update = now
+            self.frame += 1
+            if self.frame == len(explosion_anim[self.size]):
+                self.kill()
+            else:
+                center = self.rect.center
+                self.image = explosion_anim[self.size][self.frame]
+                self.rect = self.image.get_rect()
+                self.rect.center = center
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        ## scale the player img down
+        self.image = pygame.transform.scale(player_img, (50, 38))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.radius = 20
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0 
+        self.shield = 100
+        self.shoot_delay = 250
+        self.last_shot = pygame.time.get_ticks()
+        self.lives = 3
+        self.hidden = False
+        self.hide_timer = pygame.time.get_ticks()
+        self.power = 1
+        self.power_timer = pygame.time.get_ticks()
+
+    def update(self):
+        ## time out for powerups
+        if self.power >=2 and pygame.time.get_ticks() - self.power_time > POWERUP_TIME:
+            self.power -= 1
+            self.power_time = pygame.time.get_ticks()
+        ## unhide 
+        if self.hidden and pygame.time.get_ticks() - self.hide_timer > 1000:
+            self.hidden = False
+            self.rect.centerx = WIDTH / 2
+            self.rect.bottom = HEIGHT - 30
+        self.speedx = 0 #untuk gerakan player set 0 agar gerakan default
+        #Gerakan kekiri dan kekanan menggunakan keyboard
+        keystate = pygame.key.get_pressed()     
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -5
+        elif keystate[pygame.K_RIGHT]:
+            self.speedx = 5
+        #tombol untuk menembak
+        if keystate[pygame.K_SPACE]:
+            self.shoot()
+        ## check for the borders at the left and right
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+        self.rect.x += self.speedx
+
+    def shoot(self):
+
+        now = pygame.time.get_ticks()
+        if now - self.last_shot > self.shoot_delay:
+            self.last_shot = now
+            if self.power == 1:
+                bullet = Bullet(self.rect.centerx, self.rect.top)
+                all_sprites.add(bullet)
+                bullets.add(bullet)
+                shooting_sound.play()
+    def hide(self):
+        self.hidden = True
+        self.hide_timer = pygame.time.get_ticks()
+        self.rect.center = (WIDTH / 2, HEIGHT + 200)
+
+
+
+
+# musuh (meteor)
+class meteor(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image_orig = random.choice(meteor_images)
+        self.image_orig.set_colorkey(BLACK)
+        self.image = self.image_orig.copy()
+        self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width *.90 / 2)
+        self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-150, -100)
+        self.speedy = random.randrange(5, 10)        ## random kecepatan untuk meteor 
+        self.speedx = random.randrange(-2, 2)
+
+        ## menambahkan rotasi pada meteor
+        self.rotation = 0
+        self.rotation_speed = random.randrange(-8, 8)
+        self.last_update = pygame.time.get_ticks()  ## menempatkan waktu rotasi saat muncul
+        
+    def rotate(self):
+        time_now = pygame.time.get_ticks()
+        if time_now - self.last_update > 50: # in milliseconds
+            self.last_update = time_now
+            self.rotation = (self.rotation + self.rotation_speed) % 360 
+            new_image = pygame.transform.rotate(self.image_orig, self.rotation)
+            old_center = self.rect.center
+            self.image = new_image
+            self.rect = self.image.get_rect()
+            self.rect.center = old_center
+
+    def update(self):
+        self.rotate()
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        
+        if (self.rect.top > HEIGHT + 10) or (self.rect.left < -25) or (self.rect.right > WIDTH + 20):
+            self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 6)        ## random dan kecepatan meteor
+
+
+## defines the sprite for bullets
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bullet_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        ## place the bullet according to the current position of the player
+        self.rect.bottom = y 
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        """should spawn right in front of the player"""
+        self.rect.y += self.speedy
+        ## kill the sprite after it moves over the top border
+        if self.rect.bottom < 0:
+            self.kill()
+
+        
+
+
+#asset gambar ikon
+background = pygame.image.load(path.join(img_dir, 'mountains.png')).convert()
+background_rect = background.get_rect()
+
+player_img = pygame.image.load(path.join(img_dir, 'pesawat9.png')).convert()
+player_mini_img = pygame.transform.scale(player_img, (25, 19))
+player_mini_img.set_colorkey(BLACK)
+bullet_img = pygame.image.load(path.join(img_dir, 'misil3.png')).convert()
+#Load gambar meteor = meteor_img = pygame.image.load(path.join(img_dir, 'meteorBrown_med1.png')).convert()
+meteor_images = []
+meteor_list = [
+    'meteorBrown_big1.png',
+    'meteorBrown_big2.png', 
+    'meteorBrown_med1.png', 
+    'meteorBrown_med3.png',
+    'meteorBrown_small1.png',
+    'meteorBrown_small2.png',
+    'meteorBrown_tiny1.png'
+
+]
+
+for image in meteor_list:
+    meteor_images.append(pygame.image.load(path.join(img_dir, image)).convert())
+
+## ledakan meeteor
+explosion_anim = {}
+explosion_anim['lg'] = []
+explosion_anim['sm'] = []
+explosion_anim['player'] = []
+for i in range(9):
+    filename = 'regularExplosion0{}.png'.format(i)
+    img = pygame.image.load(path.join(img_dir, filename)).convert()
+    img.set_colorkey(BLACK)
+    ## Ukuran Ledakan
+    img_lg = pygame.transform.scale(img, (75, 75))
+    explosion_anim['lg'].append(img_lg)
+    img_sm = pygame.transform.scale(img, (32, 32))
+    explosion_anim['sm'].append(img_sm)
+
+    ## Ledakan Pesawat
+    filename = 'sonicExplosion0{}.png'.format(i)
+    img = pygame.image.load(path.join(img_dir, filename)).convert()
+    img.set_colorkey(BLACK)
+    explosion_anim['player'].append(img)
+
+
+### Load all game sounds
+shooting_sound = pygame.mixer.Sound(path.join(sound_folder, 'pew.wav'))
+missile_sound = pygame.mixer.Sound(path.join(sound_folder, 'rocket.ogg'))
+#pygame.mixer.music.load(path.join(sound_folder, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
+pygame.mixer.music.set_volume(0.2)      ## simmered the sound down a little
+
+player_die_sound = pygame.mixer.Sound(path.join(sound_folder, 'rumble1.ogg'))
+## Game loop
+running = True
+menu_display = True
+while running:
+    if menu_display:
+        main_menu()
+        pygame.time.wait(3000)        
+        menu_display = False
+        
+        ## group all the sprites together for ease of update
+        all_sprites = pygame.sprite.Group()
+        player = Player()
+        all_sprites.add(player)
+
+        ## spawn a group of meteor
+        meteors = pygame.sprite.Group()
+        for i in range(8):      ## 8 meteors
+            # meteor_element = meteor()
+            # all_sprites.add(meteor_element)
+            # meteors.add(meteor_element)
+            newmeteor()
+
+        ## group for bullets
+        bullets = pygame.sprite.Group()
+        #### Score board variable
+        score = 0
+        
+    #1 Process input/events
+    clock.tick(FPS)     ## will make the loop run at the same speed all the time
+    for event in pygame.event.get():      
+        ## listening for the the X button at the top
+        if event.type == pygame.QUIT:
+            running = False
+
+        ## Press ESC to exit game
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+        # ## event for shooting the bullets
+        # elif event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_SPACE:
+        #         player.shoot()      ## we have to define the shoot()  function
+
+    #2 Update
+    all_sprites.update()
+    ## check if a bullet hit a meteor
+    ## now we have a group of bullets and a group of meteor
+    hits = pygame.sprite.groupcollide(meteors, bullets, True, True)
+    for hit in hits:
+        score += 50 - hit.radius         ## memberikan score meteor kecil dan besar 
+        expl = Explosion(hit.rect.center, 'lg')
+        all_sprites.add(expl)
+        newmeteor()#membuat meteeor baru
+    hits = pygame.sprite.spritecollide(player, meteors, True, pygame.sprite.collide_circle)
+    for hit in hits:
+        player.shield -= hit.radius * 2
+        expl = Explosion(hit.rect.center, 'sm')
+        all_sprites.add(expl)
+        newmeteor()
+        if player.shield <= 0: 
+            player_die_sound.play()
+            death_explosion = Explosion(player.rect.center, 'player')
+            all_sprites.add(death_explosion)
+            # running = False     ## GAME OVER 
+            player.hide()
+            player.lives -= 1
+            player.shield = 100
+    ## if player died and the explosion has finished, end game
+    if player.lives == 0 and not death_explosion.alive():
+        running = False
+        # menu_display = True
+        # pygame.display.update()
+
+    #3 Draw/render
+    screen.fill(BLACK)
+    ## draw the stargaze.png image
+    screen.blit(background, background_rect)
+    all_sprites.draw(screen)
+    draw_text(screen, str(score), 18, WIDTH / 2, 10)     ## 10px down from the screen
+    draw_shield_bar(screen, 5, 5, player.shield)
+    # Draw lives
+    draw_lives(screen, WIDTH - 100, 5, player.lives, player_mini_img)
+    pygame.display.flip()
+pygame.quit()
